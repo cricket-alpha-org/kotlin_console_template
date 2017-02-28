9faac7df5c9242283ffe9b011b590e6aef5e14ce
Mate Herber <mate.herber@skyscanner.net>
2017-03-01 00:13:30 +0100

    Initial commit

---
+++ src/main/kotlin/org/kotlin/sample/Main.kt
@@ -0,0 +1,10 @@
+package org.kotlin.sample
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(vararg args: String) {
+            println("Hello World")
+        }
+    }
+}
